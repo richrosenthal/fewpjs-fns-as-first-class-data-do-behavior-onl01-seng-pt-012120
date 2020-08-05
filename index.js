d845b8992d9b005cbf8198898a47827dc2340401
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-05 23:44:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,7 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 
-function greet(time){
-   if (time.parseInt() < 1200) {
-     console.log("Good Morning");
-   } else if (time.parseInt() > 1200 && time.parseInt()< 1700){
-     console.log("Good Afternoon");
-   } else {
-     console.log("Good Evening");
-   }
-}
+
 
 
 /* Write your implementation of displayMessage() */
