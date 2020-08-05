ecc3e5a273a68bc7e7ef68424e1eaaef60ff0984
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-05 23:28:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+   if (time.parseInt() < 1200) {
+     console.log("Good Morning");
+   } else if (time.parseInt() > 1200 && time.parseInt()< 1700){
+     console.log("Good Afternoon");
+   } else {
+     console.log("Good Evening");
+   }
+}
+
+
 /* Write your implementation of displayMessage() */
+document.querySelector("greeting").addEventListener("click", handleClick);
+
+function handleClick(e) {
+  const timeString = document.getElementById('time').value
+  displayMessage(greet(timeString))
+}
