534d034efd60c4282960ba85101cde796ff1f47a
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-06 00:01:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,3 +30,8 @@ function greet(oldTime){
 
 
 /* Write your implementation of displayMessage() */
+function displayMessage(time) {
+  element = document.querySelector("h1#greeting")
+  element.innerHTML = greet(time)
+  
+}
